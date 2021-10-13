7d540eeb1ad08554dc3476f7e817fe05fb39798d
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-13 10:07:15 +0530

    add:intilizing card

--- src/com/bl/deckofcards/DeckCardMain.java
+++ src/com/bl/deckofcards/DeckCardMain.java
@@ -2,9 +2,27 @@ package com.bl.deckofcards;
 
 public class DeckCardMain {
 
+	int[] deck = new int[52];
+	String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+	String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+	public void shuffel() {
+		// Initialize cards
+		for (int i = 0; i < deck.length; i++) {
+			deck[i] = i;
+		}
+		for (int i = 0; i < deck.length; i++) {
+			int index = (int) (Math.random() * deck.length);
+			int temp = deck[i];
+			deck[i] = index;
+			index = temp;
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Deck of cards");
+		DeckCardMain deackCard = new DeckCardMain();
+		deackCard.shuffel();
 	}
-
 }
