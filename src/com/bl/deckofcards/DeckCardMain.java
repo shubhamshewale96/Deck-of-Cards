379c666d62b51264a8ce9667eb116a0e8c552685
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-13 08:51:40 +0530

    welcome to deck of cards

---
+++ src/com/bl/deckofcards/DeckCardMain.java
@@ -0,0 +1,10 @@
+package com.bl.deckofcards;
+
+public class DeckCardMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Deck of cards");
+	}
+
+}
